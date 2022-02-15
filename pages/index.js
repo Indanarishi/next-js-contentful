c72e4e3ef274d63889fbff2420abd02cec93695a
Jundi <indana6969@gmail.com>
2022-02-15 07:27:40 +0700

    show all recipes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { createClient } from "contentful"
+import RecipeCard from "../components/RecipeCard"
 
 export default function Home({recipes}) {
-  console.log(recipes)
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map(recipe => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
     </div>
   )
 }
