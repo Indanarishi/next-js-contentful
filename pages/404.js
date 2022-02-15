cf43fa93be59135f65fa642feea3ee9bd4ee8580
Jundi <indana6969@gmail.com>
2022-02-15 14:27:43 +0700

    add automatic redirect

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,21 @@
 import Link from "next/link"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 
 const NotFound = () => {
+    const router = useRouter()
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+        }, 3000)
+    }, [])
+
     return (
         <div className="not-found">
             <h1>404</h1>
             <h2>Ooops! That page cannot be found :(</h2>
-            <p>Redirecting to <Link href='/'>Homepage</Link></p>
+            <p>Redirecting to <Link href='/'>Homepage</Link>...</p>
 
             <style jsx>{`
                 .not-found {
