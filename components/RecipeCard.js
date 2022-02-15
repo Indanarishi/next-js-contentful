e9d234056e7f6a55358184fe46cabd0941eccd02
Jundi <indana6969@gmail.com>
2022-02-15 10:35:50 +0700

    display image from contentful

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 
 const RecipeCard = ({recipe}) => {
     const { title, slug, cookingTime, thumbnail } = recipe.fields
@@ -6,7 +7,12 @@ const RecipeCard = ({recipe}) => {
     return (
         <div className="card">
             <div className="featured">
-                {/* image thumb */}
+                <Image
+                    src={`https:${thumbnail.fields.file.url}`}
+                    width={thumbnail.fields.file.details.image.width}
+                    height={thumbnail.fields.file.details.image.height}
+                    alt={title}
+                />
             </div>
             <div className="content">
                 <div className="info">
